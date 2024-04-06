6fd7fce4fdc75a5c2979e4960c6d6def741a31d1
Nina Nowocien <nina.nowocien@gmail.com>
2024-04-06 11:08:22 +0200

    Zadanie 4 - in progres

---
+++ circum/circum_calc.py
@@ -0,0 +1,9 @@
+import math
+def obwod_kola (r):
+    return 2*math.pi*r
+def obwod_kwadratu (a):
+    return 4*a
+def obwod_prostokata (a, b):
+    return 2*a+2*b
+def obwod_trojkata (a, b, c):
+    return a+b+2*c 
