943e741501168d5cdc2e4a8f5f18dc215971877f
Zuzanna Walczak <81939@student.ue.poznan.pl>
2024-04-19 20:11:35 +0200

    Dodano Zadanie_5

---
+++ volume/volume_calc.py
@@ -0,0 +1,8 @@
+def objetosc_szescianu(a):
+    return a*a*a
+def objetosc_prostopadloscianu(a, b, c):
+    return a*b*c
+def objetosc_graniastoslupa(pole_podstawy, wysokosc_graniastoslupa):
+    return pole_podstawy*wysokosc_graniastoslupa
+def objetosc_stozka(wysokosc_stozka, promien_podstawy):
+    return 1/3*wysokosc_stozka*promien_podstawy
